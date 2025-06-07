a9584a4646132bf8f7da48776b688b35c7f56731
ABHAY <oldabhay219@gmail.com>
2025-06-07 16:40:00 +0530

    Homepage Made

--- src/firebase.js
+++ src/firebase.js
@@ -7,14 +7,15 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDmFDAlvfYfA_JIdOJo0Mkb1riQXeHiNl4",
-  authDomain: "spotlight-a7513.firebaseapp.com",
-  projectId: "spotlight-a7513",
-  storageBucket: "spotlight-a7513.firebasestorage.app",
-  messagingSenderId: "928694743277",
-  appId: "1:928694743277:web:e7346d23ead3351c461ce4",
-  measurementId: "G-3032Y8NPNR"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
